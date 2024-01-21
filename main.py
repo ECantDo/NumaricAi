ebe55fc9fef134850b8f8c57ba2b0da9117642c8
ECantDo <eric.opseth@gmail.com>
2024-01-20 18:00:42 -0600

    Initial file creation

---
+++ main.py
@@ -0,0 +1,6 @@
+import numpy as np
+from node import Node
+
+nodes = [[]]
+
+
---
+++ nodeGraph.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+class NodeGraph:
+    """
+
+    """
+    def __init__(self):
+        self.weights = None
+        self.biases = None
+
+        pass
+    pass
