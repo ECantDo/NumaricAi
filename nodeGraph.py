df3b1affa9e60e4cdf222a081446f0705b3090a5
ECantDo <eric.opseth@gmail.com>
2024-01-23 18:54:28 -0600

    Changed the sigmoid to between 0 and 1, it does not need to be between -1 and 1

--- nodeGraph.py
+++ nodeGraph.py
@@ -3,26 +3,26 @@ import numpy as np
 
 class NodeGraph:
     """
-
+    The matrix for the nural network
     """
 
     def __init__(self):
         self.weights = None
         self.biases = None
+        self.activations = None
 
         pass
 
     @staticmethod
     def sigmoid(x: float, slope: float = 1) -> float:
         """
-        Preforms the sigmoid function on some input value, but maps the output between -1 and 1 instead of the typical 0
-        and 1.
+        Preforms the sigmoid function on some input value
         :param x: The value to preform on.
         :param slope: Modifies the slope of the sigmoid function.  Must be positive. slope>1 -> gets more steep
-        :return: A value between -1 and 1.
+        :return: A value between 0 and 1.
         """
         slope = abs(slope)
-        return (2 / (1 + np.e ** (-slope * x))) - 1
+        return 1 / (1 + np.e ** (-slope * x))
         pass
 
     pass
