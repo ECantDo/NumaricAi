8111ccaab9bb3cba4ab802805e71e1f7fba003d6
ECantDo <eric.opseth@gmail.com>
2024-01-23 23:51:23 -0600

    Finished matrix multiplication single layer

--- firstAttempt/firstAttempt.py
+++ firstAttempt/firstAttempt.py
@@ -2,12 +2,14 @@ import getNumbers
 import numpy as np
 import ast
 
+import nodeGraph
+
 
 class CantDoAI:
     def __init__(self):
         self.biases = CantDoAI.get_values("biases.arr")
         self.weights = CantDoAI.get_values("weights.arr")
-        self.layers = [[0] * len(self.weights[0])]
+        self.layers = [np.array([0] * len(self.weights[0]))]
         for bias_layer in self.biases:
             self.layers.append([0] * len(bias_layer))
         pass
@@ -40,7 +42,9 @@ class CantDoAI:
         if layer_idx < 0 and layer_idx < len(self.biases):
             return
 
-        self.layers[layer_idx + 1][0] = 0
+        self.layers[layer_idx + 1] = (nodeGraph.sigmoid(np.add(
+            np.dot(np.array(self.weights[layer_idx]).T, np.array(self.layers[layer_idx])),
+            self.biases[layer_idx])).tolist())
 
         pass
 
@@ -48,8 +52,8 @@ class CantDoAI:
 
 
 def matrix_mult_example():
-    matrix_A = np.array([1, 2, 3])
-    matrix_B = np.array([1, 2, 3])
+    matrix_A = np.array([[1, 2, 3], [3, 4, 3]])
+    matrix_B = np.array([[1, 2], [3, 4], [5, 6]])
 
     # Perform matrix multiplication using numpy.dot
     result_dot = np.dot(matrix_A, matrix_B)
@@ -72,7 +76,9 @@ def matrix_mult_example():
 
 
 ai = CantDoAI()
-ai.set_intput([1, 2, 3, 4])
-ai.print_bwl()
+# ai.set_intput([.0117647059, .0705882353, .0705882353, .0705882353])
+# ai.print_bwl()
+# ai.multiply_layer(0)
+# ai.print_bwl()
 
 # matrix_mult_example()
