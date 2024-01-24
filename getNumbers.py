ddd52a85bc36e18ccf970a903c2d1451eda25a13
ECantDo <eric.opseth@gmail.com>
2024-01-23 19:20:09 -0600

    MNISIT number input setup

---
+++ getNumbers.py
@@ -0,0 +1,28 @@
+def get_data(file_name: str):
+    """
+    Gets the data from a csv file representing a gray-scale image
+    :param file_name:
+    :return:
+    """
+    pass
+
+
+def make_image():
+    """
+    Makes and displays the image of a given case, for the user to see -> this is not for the AI
+    :return:
+    """
+    pass
+
+
+root_folder_location = "E:/MINSIT (Labeled numbers)/MNIST_CSV/"
+
+
+class TestingData:
+    file_location = f"{root_folder_location}mnist_test.csv"
+    pass
+
+
+class TrainingData:
+    file_location = f"{root_folder_location}mnist_train.csv"
+    pass
