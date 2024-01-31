1d2b58c3497aacb43084d2f31750f97c2c311667
ECantDo <eric.opseth@gmail.com>
2024-01-30 19:01:38 -0600

    Start on attempt 2

--- basicFunctions.py
+++ basicFunctions.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def sigmoid(x):
+def activation(x):
     """
     Preforms the sigmoid function on some input value
     :param x: The value to preform on.
@@ -18,15 +18,29 @@ def sigmoid(x):
     pass
 
 
-def sigmoid_derivative(x):
-    s = sigmoid(x)
-    return s * (1 - s)
+def activation_derivative(x):
+    active = activation(x)
+    return active * (1 - active)
+    pass
+
+
+def slope(activation: float, expected_activation: float):
+    return 2 * (activation - expected_activation)
+    pass
+
+
+def node_cost(activation: float, expected_activation: float):
+    # cost = (a - y)^2
+    return (activation - expected_activation) ** 2
     pass
 
 
 if __name__ == "__main__":
-    operations = np.array([0, 999, 2, -999, -99])
-    print(operations.tolist())
-    print(sigmoid(operations).tolist())
-    print(sigmoid_derivative(operations).tolist())
+    # operations = np.array([0, 999, 2, -999, -99])
+    # print(operations.tolist())
+    # print(sigmoid(operations).tolist())
+    # print(sigmoid_derivative(operations).tolist())
+
+    output = [1, 1, 1, 1, 1, 1, 1]
+    correct_idx = 1
     pass
