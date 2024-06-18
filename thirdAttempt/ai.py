6d8b4aa4904e5509f5750073e25ef578dbaf9df5
ECanDo <ecandoecantdo@gmail.com>
2024-06-18 13:01:46 -0600

    Doc strings

--- thirdAttempt/ai.py
+++ thirdAttempt/ai.py
@@ -4,24 +4,26 @@ import numpy as np
 class CantDoAi:
 
     def __init__(self, weights: list[int], biases: list[int], links: list[int]):
+        # One weight for every 2 links
         self.weights = weights  # [1, 2, 2, 1, 0, 1]
+        # 1 bias for each node
         self.biases = biases  # [0] * len(self.weights)
         self.links = links  # [0, 2, 0, 3, 1, 2, 1, 3, 2, 5, 3, 5, 3, 4, 5, 4]
 
-        # Update order is the order in which the nodes should be processed; None if not initialized
-        self.update_order = None
         # Input nodes are just nodes that don't have any inputs; so they are the nodes that receive the inputs
         self.input_nodes = None
-        self.update_update_order()
+        self.update_order = self.get_update_order()
         pass
 
     # ==================================================================================================================
     # FUNCTIONS TO UPDATE INTERNAL STATES NEEDED FOR THE AI
     # ==================================================================================================================
-    def update_update_order(self) -> list[int]:
+    def get_update_order(self) -> list[int]:
         """
-        Updates the update order of processing of the nodes in the AI
-        :return:
+        Updates the update order of processing of the nodes in the AI.
+        Updates the input nodes if they have not been initialized yet.
+        Assumes that the links making the graph, make an acyclic graph.
+        :return: The processing order of the nodes in the AI
         """
         order = []
         # Check if the input nodes have been initialized
@@ -34,9 +36,6 @@ class CantDoAi:
         edgeless = list(self.input_nodes)
         edges = list(self.links)
         while len(edgeless) > 0:
-            # print(f"\n{edgeless = }")
-            # print(f"{edges = }")
-            # print(f"{order = }")
             new_edges = []
             while len(edgeless) > 0:
                 node = edgeless[0]
@@ -51,17 +50,22 @@ class CantDoAi:
                         new_edges.append(edges[i + 1])
 
                 edges = new_edges
-                # print(f"{new_edges = }")
                 new_edges = []
 
-            # print(f"{self.get_leading_nodes(edges, order) = }\n")
             edgeless = self.get_leading_nodes(edges, order)
 
         # print(f"Final order: {order}")
         return order
 
     def get_leading_nodes(self, edges: list[int] = None, exclude_input_nodes: list[int] = ()) -> list[int]:
-        input_nodes = [True] * len(self.weights)
+        """
+        Returns the leading nodes of the AI
+        :param edges: Use this to specify a custom list of edges
+        :param exclude_input_nodes: Use this to remove nodes from the leading nodes; needs the custom list of edges to
+        work properly
+        :return: The leading nodes of the AI
+        """
+        input_nodes = [True] * len(self.biases)
         if edges is None:
             edges = self.links
         for i in range(1, len(edges), 2):
@@ -71,6 +75,10 @@ class CantDoAi:
         return [i for i, x in enumerate(input_nodes) if x]
 
     def update_input_nodes(self):
+        """
+        Updates the input nodes of the AI, using the @get_leading_nodes function
+        :return:
+        """
         # Get all the nodes that don't have any inputs in the default state
         self.input_nodes = self.get_leading_nodes(None, [])
 
@@ -90,5 +98,4 @@ class CantDoAi:
 if __name__ == "__main__":
     ai = CantDoAi()
     ai.update_input_nodes()
-    ai.update_update_order()
     pass
