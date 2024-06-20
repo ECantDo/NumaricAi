01b5edd6fad00f4aa184e1cc13baacefe96b35fa
ECanDo <ecandoecantdo@gmail.com>
2024-06-20 15:55:16 -0600

    Added a basic test to see if the thinking is working

--- thirdAttempt/aiWrapper.py
+++ thirdAttempt/aiWrapper.py
@@ -4,11 +4,18 @@ from ai import CantDoAi
 
 class MicroGameWrapper:
     def __init__(self, env_name="MicroGame-v0"):
-        self.ai = CantDoAi()
+        weights, biases, links = [eval(line) for line in open("wbe.cfg").readlines()]
+        # print(weights, biases, links)
+        self.ai = CantDoAi(weights, biases, links)
         self.env_name = env_name
-        self.ai.update_update_order()
+
+        print(self.ai.think([1, 1, 1, 1, 1, 1]))
+
+        import visulizeGraph
+        visulizeGraph.main(weights)
         pass
 
 
 if __name__ == "__main__":
+    MicroGameWrapper()
     pass
