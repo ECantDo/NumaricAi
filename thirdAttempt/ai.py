a9bcd04fe3ab7cc30070d28f89c195e16a741264
ECanDo <ecandoecantdo@gmail.com>
2024-06-20 15:54:58 -0600

    Implemented memory saving reformatting

--- thirdAttempt/ai.py
+++ thirdAttempt/ai.py
@@ -3,12 +3,12 @@ import numpy as np
 
 class CantDoAi:
 
-    def __init__(self, weights: list[int], biases: list[int], links: list[int]):
+    def __init__(self, weights: list[int], biases: list[int], links: list[tuple[int, int]]):
         # One weight for every 2 links
         self.weights = weights  # [1, 2, 2, 1, 0, 1]
         # 1 bias for each node
         self.biases = biases  # [0] * len(self.weights)
-        self.links = links  # [0, 2, 0, 3, 1, 2, 1, 3, 2, 5, 3, 5, 3, 4, 5, 4]
+        self.links = links  # [(0, 2), (0, 3), (1, 2), (1, 3), (2, 5), (3, 5), (3, 4), (5, 4)]
 
         # Input nodes are just nodes that don't have any inputs; so they are the nodes that receive the inputs
         self.input_nodes = self.get_leading_nodes(None, [])
@@ -48,7 +48,7 @@ class CantDoAi:
         # print(f"Final order: {order}")
         return order
 
-    def get_leading_nodes(self, edges: list[int] = None, exclude_input_nodes: list[int] = ()) -> list[int]:
+    def get_leading_nodes(self, edges: list[tuple[int, int]] = None, exclude_input_nodes: list[int] = ()) -> list[int]:
         """
         Returns the leading nodes of the AI; i.e. the nodes that have no incoming links
         :param edges: Use this to specify a custom list of edges
@@ -59,8 +59,10 @@ class CantDoAi:
         input_nodes = [True] * len(self.biases)
         if edges is None:
             edges = self.links
-        for i in range(1, len(edges), 2):
-            input_nodes[edges[i]] = False
+        # for i in range(1, len(edges), 2):
+        #     input_nodes[edges[i]] = False
+        for edge in edges:
+            input_nodes[edge[1]] = False
         for v in exclude_input_nodes:
             input_nodes[v] = False
         return [i for i, x in enumerate(input_nodes) if x]
@@ -74,12 +76,14 @@ class CantDoAi:
         if edges is None:
             edges = self.links
         has_outgoing_connection = [False] * len(self.biases)
-        for i in range(0, len(edges), 2):
-            has_outgoing_connection[edges[i]] = True
+        # for i in range(0, len(edges), 2):
+        #     has_outgoing_connection[edges[i]] = True
+        for edge in edges:
+            has_outgoing_connection[edge[0]] = True
         return [idx for idx, cnnts in enumerate(has_outgoing_connection) if not cnnts]
         pass
 
-    def find_outgoing_links(self, node: int, edges: list[int] = None) -> list[int]:
+    def find_outgoing_links(self, node: int, edges: list[tuple[int, int]] = None) -> list[int]:
         """
         Returns the indexes of the outgoing links of a given node
         :param node: The node to find the outgoing links of
@@ -88,10 +92,11 @@ class CantDoAi:
         """
         if edges is None:
             edges = self.links
-        return [i for i, x in enumerate(edges) if x == node and i % 2 == 0]
+        # return [i for i, x in enumerate(edges) if x == node and i % 2 == 0]
+        return [i for i, edge in enumerate(edges) if edge[0] == node]
         pass
 
-    def get_subgraph(self, node: int, edges: list[int] = None) -> list[int]:
+    def get_subgraph(self, node: int, edges: list[tuple[int, int]] = None) -> list[tuple[int, int]]:
         """
         Returns the subgraph of the AI, with the given nodes removed
         :param node: The node that is not in the subgraph
@@ -102,10 +107,13 @@ class CantDoAi:
             edges = self.links
 
         new_edges = []
-        for i in range(0, len(edges), 2):
-            if edges[i] != node:
-                new_edges.append(edges[i])
-                new_edges.append(edges[i + 1])
+        # for i in range(0, len(edges), 2):
+        #     if edges[i] != node:
+        #         new_edges.append(edges[i])
+        #         new_edges.append(edges[i + 1])
+        for edge in edges:
+            if edge[0] != node:
+                new_edges.append(edge)
         return new_edges
         pass
 
@@ -133,9 +141,9 @@ class CantDoAi:
             updated_nodes = set()
             for idx in outgoing_links:
                 # TODO: Check if this works
-                other_node = self.links[idx + 1]
+                other_node = self.links[idx][1]
                 updated_nodes.add(other_node)
-                weight = self.weights[self.weights.index((node, other_node)) + 1]
+                weight = self.weights[self.links.index((node, other_node))]
                 values[other_node] += values[node] * self.weights[weight] + self.biases[other_node]
 
             for updated_node in updated_nodes:
@@ -172,10 +180,9 @@ class ActivationFunctions:
     @staticmethod
     def step(x: float) -> float:
         return 1 if x > 0 else 0
+
     pass
 
 
 if __name__ == "__main__":
-    ai = CantDoAi()
-    ai.update_input_nodes()
     pass
